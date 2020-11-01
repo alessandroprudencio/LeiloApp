659bc48696c71e4b6dfe7b80be1c1acc7405dc5b
alessandroprudencio <alessandroconectado@gmail.com>
2020-11-01 16:44:50 -0400

    Componente titulo da pagina

---
+++ components/layout/TitlePage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="mb-5">
+    <h2>{{ name }}</h2>
+  </div>
+</template>
+<script>
+export default {
+  name: 'TitlePage',
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>

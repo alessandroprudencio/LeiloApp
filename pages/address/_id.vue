182f19e6810c130a1285c280649da853a6fd5e82
alessandroprudencio <alessandroconectado@gmail.com>
2020-11-01 20:52:34 -0400

    Separando componentes

---
+++ pages/address/_id.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="10">
+      <div class="text-center">
+        <TitlePage name="Editar de Endereços" />
+        <AddressList />
+      </div>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import TitlePage from '~/components/layout/TitlePage.vue'
+import AddressList from '~/components/address/AddressList.vue'
+
+export default {
+  components: {
+    TitlePage,
+    AddressList,
+  },
+}
+</script>

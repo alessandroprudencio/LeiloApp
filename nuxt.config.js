3ccd79a9a9aea821970201e72142c142d77f79ed
alessandroprudencio <alessandroconectado@gmail.com>
2020-11-01 21:30:16 -0400

    adicionado lib de mascara para o cEP

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,10 +14,10 @@ export default {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [],
+  css: ['~/assets/main.css'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [],
+  plugins: ['~/plugins/vue-the-mask.js'],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
